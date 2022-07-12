c0d162dce1947d751da6258b1a1dde6d148ff32a
Mandy Tsang <mandy.tsang007@gmail.com>
2022-07-11 20:49:54 -0700

    adjust images sizing

--- src/components/ClickItem.js
+++ src/components/ClickItem.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import './style.css';
 
-class ClickItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick(){
-        console.log('click happened');
-    }
-    render(){
-        return (
-            <div
-                role='img'
-                aria-label='click item'
-                onClick={() => this.props.handleClick(this.props.id)}     
-                style={{backgroundImage: `url(${this.props.image})`, 'backgroundPosition': 'top'}}
-                className={`click-item${this.shake ? "shake" : ""}`}
-                >
+    class ClickItem extends React.Component {
+        constructor(props) {
+            super(props);
+            this.handleClick = this.handleClick.bind(this);
+        }
+        handleClick() {
+            console.log('click happened');
+        }
+        render() {
+            return (
+                <div>
+                    <img 
+                        style={{height: "170px", width: "150px"}}
+                        src={`${this.props.image}`}
+                        onClick={() => this.props.handleClick(this.props.id)}
+                        aria-label='click item'
+                        role='img'
+                        className={`click-item${this.shake ? "shake" : ""}`}
+                    ></img>
+
                 </div>
-        );
+                // <div
+                //     role='img'
+                //     aria-label='click item'
+                //     onClick={() => this.props.handleClick(this.props.id)}
+                //     style={{ backgroundImage: `url(${this.props.image})`, 'backgroundPosition': 'top' }}
+                //     className={`click-item${this.shake ? "shake" : ""}`}
+                // >
+                // </div>
+            );
+        };
     };
-};
 export default ClickItem;
